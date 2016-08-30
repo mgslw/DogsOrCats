b59534571795db4953af3ce9b16b3d89045f17fb
mgslw <magda.sliwinska710@gmail.com>
2016-08-30 22:47:20 +0200

    java button

--- app/src/main/java/girlscodefun/com/carordog/MainActivity.java
+++ app/src/main/java/girlscodefun/com/carordog/MainActivity.java
@@ -2,12 +2,38 @@ package girlscodefun.com.carordog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    int - integers
+    float -
+    strin -
+    boolean - prawda/fałsz
+
+     */
+    int dogVotesCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void voteForDog(View view) {
+        TextView dogVotes = (TextView) findViewById(R.id.dog_votes);
+
+        dogVotesCounter = dogVotesCounter + 1 ;
+
+        dogVotes.setText("" + dogVotesCounter);
+        /*
+        ("" + "int") zamienia liczbę (int) na napis (string) tak że może być wyświetlony
+         */
+
+    }
+
+
 }
